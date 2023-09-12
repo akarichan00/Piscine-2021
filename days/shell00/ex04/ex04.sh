dbc82557da5e1a1cb8cef2544b1c72b16101661f
akarichan00 <86488105+akarichan00@users.noreply.github.com>
2023-09-12 11:44:12 +0100

    Update ex04.sh

--- days/shell00/ex04/ex04.sh
+++ days/shell00/ex04/ex04.sh
@@ -5,8 +5,8 @@
 # there is a diff between -U and -u be careful !
 ls -trhmp
 # or 
-ls -upm
-# -U -> do not sort; list entries in directory order
+ls -tpm
+# -t -> sort by modification time, newest first
 # -p -> append / indicator to directories
 # -m -> fill width with a comma separated list of entries
 #or
