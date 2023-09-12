29cec78b4c57ee4986d8815fcf1b10f4d73884b5
akarichan00 <86488105+akarichan00@users.noreply.github.com>
2023-09-12 11:04:56 +0100

    Update ex04.sh

--- days/shell00/ex04/ex04.sh
+++ days/shell00/ex04/ex04.sh
@@ -6,6 +6,9 @@
 ls -trhmp
 # or 
 ls -upm
+# -U -> do not sort; list entries in directory order
+# -p -> append / indicator to directories
+# -m -> fill width with a comma separated list of entries
 #or
 ls -tp --format=comma #?
 #RTFM.
